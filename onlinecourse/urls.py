a6a7535cc956630500e96ce3a52958eb1deb0133
mars <shubhamshukla.shukla@gmil.com>
2024-08-12 04:36:59 -0400

    changes

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -5,9 +5,8 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
+    path('<int:course_id>/submit/', views.submit, name="submit"),
     path(route='', view=views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
